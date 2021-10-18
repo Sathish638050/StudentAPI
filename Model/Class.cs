b322ee0d3ca02b454eddf300daf039157fd67dc1
unknown <Administrator@KANINI-LTP-500.gisbizdev.gisbiz.chennai.com>
2021-10-18 22:15:25 +0530

    first commit

--- Model/Class.cs
+++ Model/Class.cs
@@ -22,11 +22,11 @@ namespace StudentApi.Model
         public async Task<List<Class>> GetStudentClass(int id)
         {
             List<Class> lc = new List<Class>();
-
+            var date = DateTime.Now;
             lc = (from i in db.CourseEnrolls
                        join j in db.Classes
                            on i.CourseId equals j.CourseId
-                       where i.UserId == id
+                       where i.UserId == id && j.ClassDate == date
                        select j).ToList();
             return lc;
         }
